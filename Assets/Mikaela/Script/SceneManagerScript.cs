bd3dfac3b502958ecf63ada705688d0403404f5d
jye <167274724+miokag@users.noreply.github.com>
2025-04-03 07:11:21 +0800

    Added UI Assets

---
+++ Assets/Mikaela/Script/SceneManagerScript.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Tutorial3DScene");
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene("__"); // to replay , add the scenename to " " 
+    }
+
+    public void Skip()
+    {
+        SceneManager.LoadScene("__"); // to skip tutorial, add the scenename to " " 
+    }
+}
